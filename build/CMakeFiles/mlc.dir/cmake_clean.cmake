9f5f2d32af0a0ec8f6c7d8ffe04b62f041a28a48
aadheeshnandan <aadheeshnandan@icloud.com>
2026-05-27 17:50:33 -0400

    setup cmake

---
+++ build/CMakeFiles/mlc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mlc.dir/src/main.c.o"
+  "CMakeFiles/mlc.dir/src/main.c.o.d"
+  "mlc"
+  "mlc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mlc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main() {
+  printf("ML Compiler Online\n");
+}
